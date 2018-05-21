e366b53dabc5ac1bc5d5fb562cff5c7d88d46b5a
m0bius <ezra.cook@u.northwestern.edu>
2018-05-21 00:21:49 -0500

    have started writing JSON obj to text to parse the calls I need

--- CLI.js
+++ CLI.js
@@ -1,4 +1,5 @@
 //require("dotenv").config();
+var fs = require('fs'); // gonna write some of this shit down
 //var request = require('request');
 //var Coinigy = require('node-coinigy');
 const shapeshift = require('shapeshift');
@@ -20,7 +21,18 @@ const coincap = require('coincap-lib');
 //----------------------------npm install node-coinigy --save
 
 
- coincap.coins().then(console.log);
+coincap.front().then(function (response) {
+    fs.writeFile("coincap.txt", JSON.stringify(response), function (err) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("coincap.txt was updated!");
+
+    });
+});
 //coincap.coins().then(console.log) // ["300", "611", "888", ...]
 //coincap.coinHistory('BTC', 1).then(console.log) // { market_cap: [...], ... }
 
@@ -30,71 +42,175 @@ const coincap = require('coincap-lib');
 
 
 //---------------------node npm install --save shapeshift
- const pair = 'btc_ltc';
+//  const pair = 'btc_ltc';
 
- shapeshift.getRate(pair)
- 	.then(function(data){
-        console.log(data);
- 	});
- shapeshift.getLimit(pair)
- 	.then(function(data){
- 		const body = data.body;
-        console.log(body);
- 		//{"pair":"btc_ltc","limit":"1.98046131"}
- 	});
+//  shapeshift.getRate(pair)
+//  	.then(function(data){
+//         console.log(data);
+//  	});
+//  shapeshift.getLimit(pair)
+//  	.then(function(data){
+//  		const body = data.body;
+//         console.log(body);
+//  		//{"pair":"btc_ltc","limit":"1.98046131"}
+//  	});
 
- shapeshift.getCoins()
- 	.then(data => {
-         const coins = data.body;
-         console.log(coins);
- 		//{"pair":"btc_ltc","limit":"1.98046131"}
- 	});
+shapeshift.getCoins()
+    .then(data => {
+        const coins = data.body;
+        fs.writeFile("shapeshift.txt", JSON.stringify(coins), function (err) {
+
+            // If the code experiences any errors it will log the error to the console.
+            if (err) {
+                return console.log(err);
+            }
+
+            // Otherwise, it will print: "movies.txt was updated!"
+            console.log("shapeshift.txt was updated!");
+            //Provides miners fees for all coins. Have to pull coin trade data individually.
+        });
+    });
 
 
 
 
 
+var Changelly = require('./lib.js');
 
+var changelly = new Changelly(
+    'ed7f576df31d4bcab7742641d37f935b',
+    '4fb2405ff40d0e7874baae70687ff7e28b3ffec8f9303aed5bb205b2da871bbe'
+);
 
-//-----------------------------------------------------------------------------------------------------------------------------------------------------
-var http = require("http");
+//will have to call for each XXXYYY (correct data)
+changelly.getExchangeAmount('btc', 'eth', 1, function (err, data) {
+    if (err) {
+        console.log('Error!', err);
+    } else {
+        fs.writeFile("changellyBTCETH.txt", JSON.stringify(data), function (err) {
 
-var options = {
-  "method": "POST",
-  "hostname": "api.changelly.com",
-  "headers": {
-    "Content-Type": "application/json",
-    "api-key": "ed7f576df31d4bcab7742641d37f935b",
-    "sign": "4fb2405ff40d0e7874baae70687ff7e28b3ffec8f9303aed5bb205b2da871bbe"
-  }
-};
+            // If the code experiences any errors it will log the error to the console.
+            if (err) {
+                return console.log(err);
+            }
 
-var req = http.request(options, function (res) {
-  var chunks = [];
+            console.log("changellyBTCETH.txt was updated!"); //good data
 
-  res.on("data", function (chunk) {
-    chunks.push(chunk);
-  });
-
-  res.on("end", function () {
-    var body = Buffer.concat(chunks);
-    console.log(body.toString());
-  });
+        });
+    }
 });
 
-req.write(JSON.stringify({ id: 'test',
-  jsonrpc: '2.0',
-  method: 'getCurrenciesFull',
-  params: {} }));
-req.end();
+changelly.getCurrencies(function (err, data) {
+    if (err) {
+        console.log('Error!', err);
+    } else {
+        fs.writeFile("changellycurrencies.txt", JSON.stringify(data), function (err) {
+
+            // If the code experiences any errors it will log the error to the console.
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("changellyminerfee.txt was updated!"); //good data
+
+        });
+    }
+});
+
+//-----------------------------------------------------------------------------------------------------------------------------------------------------
+// var http = require("http");
+
+// var options = {
+//   "method": "POST",
+//   "hostname": "api.changelly.com",
+//   "headers": {
+//     "Content-Type": "application/json",
+//     "api-key": "ed7f576df31d4bcab7742641d37f935b",
+//     "sign": "4fb2405ff40d0e7874baae70687ff7e28b3ffec8f9303aed5bb205b2da871bbe"
+//   }
+// };
+
+// var req = http.request(options, function (res) {
+//   var chunks = [];
+
+//   res.on("data", function (chunk) {
+//     chunks.push(chunk);
+//   });
+
+//   res.on("end", function () {
+//     var body = Buffer.concat(chunks);
+//     fs.writeFile("changelly.txt", JSON.stringify(body), function(err) {
+
+//         // If the code experiences any errors it will log the error to the console.
+//         if (err) {
+//           return console.log(err);
+//         }
+
+//         // Otherwise, it will print: "movies.txt was updated!"
+//         console.log("changelly.txt was updated!");
+//      //Provides miners fees for all coins. Have to pull coin trade data individually.
+//      });
+//   });
+// });
+
+// req.write(JSON.stringify({ id: 'test',
+//   jsonrpc: '2.0',
+//   method: 'getCurrenciesFull',
+//   params: {} }));
+// req.end();
 //----------------------------------------------------------------------------------------
 
 
+var getJSON = require('get-json');
 
 
 
+var get1 = new getJSON('https://api.bibox.com/v1/mdata?cmd=marketAll', function (error, response) {
+    if (error) throw error;
 
+    fs.writeFile("bibox.txt", JSON.stringify(response.result), function (err) {
 
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("bibox.txt was updated!"); //good data
+
+    });
+});
+
+//-----------------------PROBLEM (PROMISES??) -------------------- The first call of this NPM works, the second two return undefined. Works in postman.
+
+var get2 = new getJSON('https://www.bit-z.com/api_v1/tickerall', function (error, response) {
+    if (error) throw error;
+
+    fs.writeFile("bitz.txt", JSON.stringify(response.result), function (err) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("bitz.txt was updated!"); //good data
+
+    });
+});
+
+var get3 = new getJSON('https://api.kucoin.com/v1/open/tick', function (error, response) {
+    if (error) throw error;
+
+    fs.writeFile("kucoin.txt", JSON.stringify(response.result), function (err) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("kucoin.txt was updated!"); //good data
+
+    });
+});
 
 //this is the main engine for doing the math and finding the pairs. I will write prototypes and include them here, as well as any node NPM
 
