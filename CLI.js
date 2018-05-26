0b260dcbae672d080ef556643f3bc0507b73c4be
m0bius <ezra.cook@u.northwestern.edu>
2018-05-25 21:24:03 -0500

    i'm drinking your milkshake. next i have to make some promises

--- CLI.js
+++ CLI.js
@@ -1,30 +1,57 @@
 //require("dotenv").config();
 var fs = require('fs'); // gonna write some of this shit down
 //var request = require('request');
-//var Coinigy = require('node-coinigy');
 const shapeshift = require('shapeshift');
 const coincap = require('coincap-lib');
 //var keys = require("./keys.js");
 //var coinigy = new Coinigy(keys.coinigy);
 
-
+var coinigyData = [];
+var coincapData = [];
+var biboxData = [];
+var kucoinData = [];
+var bitzData = [];
 //----------------------------Coinigy contains private key data that I would like to keep secure, but it also contains all the data needed for the major exchanges in one place. I would like to use it if possible.
-// coinigy.activity()
-// .then(function (body) {
-//   console.log(body.data);
-//   console.log(body.notifications);
-// })
-// .catch(function (err) {
-//   console.log(err);
-// });
+var request = require('request');
 
-//----------------------------npm install node-coinigy --save
+request({
+  method: 'POST',
+  url: 'https://api.coinigy.com/api/v1/userWatchList',
+  headers: {
+    'Content-Type': 'application/json',
+    'X-API-KEY': '2f1198cd7540ad52b584cfcc21a1e476',
+    'X-API-SECRET': 'f021026e7281dfcb05220dfe299e38b6'
+  }}, function (error, response, body) {
+      if(error) throw error;
+    fs.writeFile("coinigy.txt", body, function (err) {
+
+        // If the code experiences any errors it will log the error to the console.
+        if (err) {
+            return console.log(err);
+        }
+
+        console.log("coinigy.txt was updated!");
+
+    });
+    //------------NEW CODE------------GRABBING FIRST SET OF DATA
+     
+    JSON.parse(body).data.forEach(function(element) {
+        coinigyData.push({
+            "mkt_name": element.mkt_name,
+            "exch_code": element.exch_code,
+            "exch_name": element.exch_name,
+            "primary_currency_name": element.primary_currency_name,
+            "secondary_currency_name": element.secondary_currency_name,
+            "last_price": element.last_price
+        });
+    });
+    //good data. needs additions from coincap
+});
 
 
 coincap.front().then(function (response) {
     fs.writeFile("coincap.txt", JSON.stringify(response), function (err) {
 
-        // If the code experiences any errors it will log the error to the console.
         if (err) {
             return console.log(err);
         }
@@ -32,13 +59,129 @@ coincap.front().then(function (response) {
         console.log("coincap.txt was updated!");
 
     });
+    response.find(function(element){
+        if(element.short === "BTC") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "ETH") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "BCH") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "DASH") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "LTC") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "NEO") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "STEEM") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "XLM") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "XMR") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "XRP") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    response.find(function(element){
+        if(element.short === "ZEC") {
+            coincapData.push(
+                {
+                    "cap24hrChange": element.cap24hrChange,
+                    "short": element.short,
+                    "price": element.price
+                }
+            );
+        }
+    });
+    //good data
 });
-//coincap.coins().then(console.log) // ["300", "611", "888", ...]
-//coincap.coinHistory('BTC', 1).then(console.log) // { market_cap: [...], ... }
-
-//coincap will provide USD data as a constant point of evaluation. It will be compared to other tether type coins.
-//1h global % increase and decrease are also useful. As well as average
-//---------------------node npm install coincap-lib
 
 
 //---------------------node npm install --save shapeshift
@@ -117,49 +260,6 @@ changelly.getCurrencies(function (err, data) {
     }
 });
 
-//-----------------------------------------------------------------------------------------------------------------------------------------------------
-// var http = require("http");
-
-// var options = {
-//   "method": "POST",
-//   "hostname": "api.changelly.com",
-//   "headers": {
-//     "Content-Type": "application/json",
-//     "api-key": "ed7f576df31d4bcab7742641d37f935b",
-//     "sign": "4fb2405ff40d0e7874baae70687ff7e28b3ffec8f9303aed5bb205b2da871bbe"
-//   }
-// };
-
-// var req = http.request(options, function (res) {
-//   var chunks = [];
-
-//   res.on("data", function (chunk) {
-//     chunks.push(chunk);
-//   });
-
-//   res.on("end", function () {
-//     var body = Buffer.concat(chunks);
-//     fs.writeFile("changelly.txt", JSON.stringify(body), function(err) {
-
-//         // If the code experiences any errors it will log the error to the console.
-//         if (err) {
-//           return console.log(err);
-//         }
-
-//         // Otherwise, it will print: "movies.txt was updated!"
-//         console.log("changelly.txt was updated!");
-//      //Provides miners fees for all coins. Have to pull coin trade data individually.
-//      });
-//   });
-// });
-
-// req.write(JSON.stringify({ id: 'test',
-//   jsonrpc: '2.0',
-//   method: 'getCurrenciesFull',
-//   params: {} }));
-// req.end();
-//----------------------------------------------------------------------------------------
-
 
 var getJSON = require('get-json');
 
@@ -170,7 +270,6 @@ var get1 = new getJSON('https://api.bibox.com/v1/mdata?cmd=marketAll', function 
 
     fs.writeFile("bibox.txt", JSON.stringify(response.result), function (err) {
 
-        // If the code experiences any errors it will log the error to the console.
         if (err) {
             return console.log(err);
         }
@@ -178,9 +277,97 @@ var get1 = new getJSON('https://api.bibox.com/v1/mdata?cmd=marketAll', function 
         console.log("bibox.txt was updated!"); //good data
 
     });
+    response.result.find(function(element){
+        if(element.coin_symbol === "BCH" && element.currency_symbol === "BTC") {
+            biboxData.push(
+                {
+                    "coin_symbol": element.coin_symbol,
+                    "currency_symbol": element.currency_symbol,
+                    "last": element.last
+                }
+            );
+        }
+    });
+    response.result.find(function(element){
+        if(element.coin_symbol === "BCH" && element.currency_symbol === "ETH") {
+            biboxData.push(
+                {
+                    "coin_symbol": element.coin_symbol,
+                    "currency_symbol": element.currency_symbol,
+                    "last": element.last
+                }
+            );
+        }
+    });
+    response.result.find(function(element){
+        if(element.coin_symbol === "DASH" && element.currency_symbol === "BTC") {
+            biboxData.push(
+                {
+                    "coin_symbol": element.coin_symbol,
+                    "currency_symbol": element.currency_symbol,
+                    "last": element.last
+                }
+            );
+        }
+    });
+    response.result.find(function(element){
+        if(element.coin_symbol === "DASH" && element.currency_symbol === "ETH") {
+            biboxData.push(
+                {
+                    "coin_symbol": element.coin_symbol,
+                    "currency_symbol": element.currency_symbol,
+                    "last": element.last
+                }
+            );
+        }
+    });
+    response.result.find(function(element){
+        if(element.coin_symbol === "LTC" && element.currency_symbol === "BTC") {
+            biboxData.push(
+                {
+                    "coin_symbol": element.coin_symbol,
+                    "currency_symbol": element.currency_symbol,
+                    "last": element.last
+                }
+            );
+        }
+    });
+    response.result.find(function(element){
+        if(element.coin_symbol === "LTC" && element.currency_symbol === "ETH") {
+            biboxData.push(
+                {
+                    "coin_symbol": element.coin_symbol,
+                    "currency_symbol": element.currency_symbol,
+                    "last": element.last
+                }
+            );
+        }
+    });
+    response.result.find(function(element){
+        if(element.coin_symbol === "NEO" && element.currency_symbol === "BTC") {
+            biboxData.push(
+                {
+                    "coin_symbol": element.coin_symbol,
+                    "currency_symbol": element.currency_symbol,
+                    "last": element.last
+                }
+            );
+        }
+    });
+    response.result.find(function(element){
+        if(element.coin_symbol === "NEO" && element.currency_symbol === "ETH") {
+            biboxData.push(
+                {
+                    "coin_symbol": element.coin_symbol,
+                    "currency_symbol": element.currency_symbol,
+                    "last": element.last
+                }
+            );
+        }
+    });
+    //good data
 });
 
-//-----------------------PROBLEM (PROMISES??) -------------------- The first call of this NPM works, the second two return undefined. Works in postman.
 
 var get2 = new getJSON('https://www.bit-z.com/api_v1/tickerall', function (error, response) {
     if (error) throw error;
@@ -195,6 +382,29 @@ var get2 = new getJSON('https://www.bit-z.com/api_v1/tickerall', function (error
         console.log("bitz.txt was updated!"); //good data
 
     });
+    
+
+    bitzData.push({
+        "symbol": "eth_btc",
+        "last": response.data.eth_btc.last
+    });
+    bitzData.push({
+        "symbol": "bch_btc",
+        "last": response.data.bch_btc.last
+    });
+    bitzData.push({
+        "symbol": "dash_btc",
+        "last": response.data.dash_btc.last
+    });
+    bitzData.push({
+        "symbol": "ltc_btc",
+        "last": response.data.ltc_btc.last
+    });
+    bitzData.push({
+        "symbol": "zec_btc",
+        "last": response.data.zec_btc.last
+    });
+    //good data
 });
 
 var get3 = new getJSON('https://api.kucoin.com/v1/open/tick', function (error, response) {
@@ -210,6 +420,107 @@ var get3 = new getJSON('https://api.kucoin.com/v1/open/tick', function (error, r
         console.log("kucoin.txt was updated!"); //good data
 
     });
+
+    response.data.find(function(element){
+        if(element.symbol === "ETH-BTC") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    response.data.find(function(element){
+        if(element.symbol === "BCH-BTC") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    response.data.find(function(element){
+        if(element.symbol === "BCH-ETH") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    response.data.find(function(element){
+        if(element.symbol === "DASH-BTC") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    response.data.find(function(element){
+        if(element.symbol === "DASH-ETH") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    response.data.find(function(element){
+        if(element.symbol === "LTC-BTC") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    response.data.find(function(element){
+        if(element.symbol === "LTC-ETH") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    response.data.find(function(element){
+        if(element.symbol === "NEO-BTC") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    response.data.find(function(element){
+        if(element.symbol === "NEO-ETH") {
+            kucoinData.push(
+                {
+                    "symbol": element.symbol,
+                    "lastDealPrice": element.lastDealPrice,
+                    "feeRate": element.feeRate
+                }
+            );
+        }
+    });
+    //kucoin ok
 });
 
 //this is the main engine for doing the math and finding the pairs. I will write prototypes and include them here, as well as any node NPM
