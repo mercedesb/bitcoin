60e8728e9b1642984c496047037313573e0cee10
m0bius <ezra.cook@u.northwestern.edu>
2018-06-01 16:40:40 -0500

    hello world server.js

--- server.js
+++ server.js
@@ -15,84 +15,73 @@ app.use(express.static("public"));
 // Sets up the Express app to handle data parsing
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static("public"));
 
-var exphbs = require("express-handlebars");
+app.get("/", function(req, res){
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-// Serve index.handlebars to the root route.
-app.get("/", function(req, res) {
-    connection.query("SELECT * FROM quotes;", function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
-  
-      res.render("index", { quotes: data });
-    });
-  });
-  
   // Show the user the individual quote and the form to update the quote.
-  app.get("/:id", function(req, res) {
-    connection.query("SELECT * FROM quotes where id = ?", [req.params.id], function(err, data) {
-      if (err) {
-        return res.status(500).end();
-      }
+  // app.get("/:id", function(req, res) {
+  //   connection.query("SELECT * FROM quotes where id = ?", [req.params.id], function(err, data) {
+  //     if (err) {
+  //       return res.status(500).end();
+  //     }
   
-      console.log(data);
-      res.render("single-quote", data[0]);
-    });
-  });
+  //     console.log(data);
+  //     res.render("single-quote", data[0]);
+  //   });
+  // });
   
-  app.post("/api/quotes", function(req, res) {
-    connection.query("INSERT INTO quotes (author, quote) VALUES (?, ?)", [req.body.author, req.body.quote], function(
-      err,
-      result
-    ) {
-      if (err) {
-        // If an error occurred, send a generic server failure
-        return res.status(500).end();
-      }
+  // app.post("/api/quotes", function(req, res) {
+  //   connection.query("INSERT INTO quotes (author, quote) VALUES (?, ?)", [req.body.author, req.body.quote], function(
+  //     err,
+  //     result
+  //   ) {
+  //     if (err) {
+  //       // If an error occurred, send a generic server failure
+  //       return res.status(500).end();
+  //     }
   
-      // Send back the ID of the new quote
-      res.json({ id: result.insertId });
-    });
-  });
+  //     // Send back the ID of the new quote
+  //     res.json({ id: result.insertId });
+  //   });
+  // });
   
-  app.delete("/api/quotes/:id", function(req, res) {
-    connection.query("DELETE FROM quotes WHERE id = ?", [req.params.id], function(err, result) {
-      if (err) {
-        // If an error occurred, send a generic server failure
-        return res.status(500).end();
-      }
-      else if (result.affectedRows === 0) {
-        // If no rows were changed, then the ID must not exist, so 404
-        return res.status(404).end();
-      }
-      res.status(200).end();
+  // app.delete("/api/quotes/:id", function(req, res) {
+  //   connection.query("DELETE FROM quotes WHERE id = ?", [req.params.id], function(err, result) {
+  //     if (err) {
+  //       // If an error occurred, send a generic server failure
+  //       return res.status(500).end();
+  //     }
+  //     else if (result.affectedRows === 0) {
+  //       // If no rows were changed, then the ID must not exist, so 404
+  //       return res.status(404).end();
+  //     }
+  //     res.status(200).end();
   
-    });
-  });
+  //   });
+  // });
   
-  // Update a quote by an id and then redirect to the root route.
-  app.put("/api/quotes/:id", function(req, res) {
-    connection.query(
-      "UPDATE quotes SET author = ?, quote = ? WHERE id = ?",
-      [req.body.author, req.body.quote, req.params.id],
-      function(err, result) {
-        if (err) {
-          // If an error occurred, send a generic server failure
-          return res.status(500).end();
-        }
-        else if (result.changedRows === 0) {
-          // If no rows were changed, then the ID must not exist, so 404
-          return res.status(404).end();
-        }
-        res.status(200).end();
+  // // Update a quote by an id and then redirect to the root route.
+  // app.put("/api/quotes/:id", function(req, res) {
+  //   connection.query(
+  //     "UPDATE quotes SET author = ?, quote = ? WHERE id = ?",
+  //     [req.body.author, req.body.quote, req.params.id],
+  //     function(err, result) {
+  //       if (err) {
+  //         // If an error occurred, send a generic server failure
+  //         return res.status(500).end();
+  //       }
+  //       else if (result.changedRows === 0) {
+  //         // If no rows were changed, then the ID must not exist, so 404
+  //         return res.status(404).end();
+  //       }
+  //       res.status(200).end();
   
-      }
-    );
-  });
+  //     }
+  //   );
+  // });
   
   // Start our server so that it can begin listening to client requests.
   app.listen(PORT, function() {
