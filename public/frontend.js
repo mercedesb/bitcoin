16e1af1f62d8e629dfe035efac9f80d88ccec245
m0bius <ezra.cook@u.northwestern.edu>
2018-06-07 17:43:28 -0500

    presentation

--- public/frontend.js
+++ public/frontend.js
@@ -1,94 +1,57 @@
-async function buildTable() {
-    console.log("starting function: build table");
-    var table = $("<table class='table table-hover table-sm table-responsive'>");
-    var head = $("<thead id='shapeshift'>");
-    var tr = $("<tr>");
-    table.append(head);
-    head.append(tr);
+$(document).ready(function() {
 
-//Whatever happens here works
-    var data = {};
-    data.title = "title";
-    data.message = "message";
-
-    $.ajax({
-        type: 'POST',
-        data: JSON.stringify(data),
-        contentType: 'application/json',
-        url: 'http://localhost:3000/shapeshift',
-        success: function (data) {
-            console.log('success');
-            console.log(JSON.stringify(data));
+  function buildTable() {
+        console.log("starting function: build table");
+        var table = $("<table class='table table-hover table-sm table-responsive'>");
+        var head = $("<thead id='shapeshift'>");
+        var tr = $("<tr>");
+        table.append(head);
+        head.append(tr);
+        var shapeshift = JSON.parse(localStorage.getItem("shapeshift"));
+        //Here I put the first coin
+        var keys = Object.keys(shapeshift);
+        for (var i = 0; i < keys.length; i++) {
+            addTH(tr, keys[i].slice(0 - 4));
         }
-    });
+        table.append(tbody);
 
-    var shapeshiftData;
+        makeRow(tbody, shapeshift);
 
-    $.ajax({
-        type: "GET",
-        url: "localhost:3000/shapeshift",
-        datatype : "application/json",
-        contentType: "text/plain",
-        success: function (response) {
-            console.log(JSON.parse(response));
-            shapeshiftData = JSON.parse(response);
-   
-    //var shapeshift = JSON.parse(localStorage.getItem("shapeshift"));
-    //Here I put the first coin
-    var keys = Object.keys(shapeshiftData);
-        for(var i = 0; i<keys.length; i++) {
-        addTH(tr, keys[i].slice(0 - 4));
+        $("#shapeshift").append(table);
     }
-    table.append(tbody);
-
-    makeRow(tbody, shapeshift);
-
-    console.log(success);
-    $("#coin1").append(table);
-}
-});
-
-}
 
 function addTH(x, y) {
-    var th = $("<th scope='col'>");
-    th.text(y);
-    x.append(th);
-}
-function addTD(x, y) {
-    var td = $("<td>");
-    td.text(y);
-    x.append(td);
-}
-function addTH2(x, y) {
-    var th = $("<th scope = row")
-    th.text(y);
-    x.append(th);
-}
-function makeRow(body, obj) {
-    var tr = $("<tr>");
-    var th = $("<th scope = 'row'>");
-
-
-
-    var keys = Object.keys(shapeshift);
-    for (var i = 0; i < keys.length; i++) {
-        addTH2(tr, keys[i]);
-        addTD(tr, shapeshift[keys[i]])
+        var th = $("<th scope='col'>");
+        th.text(y);
+        x.append(th);
     }
-    body.append(tr);
-}
+function addTD(x, y) {
+        var td = $("<td>");
+        td.text(y);
+        x.append(td);
+    }
+function addTH2(x, y) {
+        var th = $("<th scope = row")
+        th.text(y);
+        x.append(th);
+    }
+function makeRow(body, obj) {
+        var tr = $("<tr>");
+        var th = $("<th scope = 'row'>");
 
-buildTable();
+        var shapeshift = JSON.parse(localStorage.getItem("shapeshift"));
+        //Here I put the first coin
+        var keys = Object.keys(shapeshift);
+        for (var i = 0; i < keys.length; i++) {
+            addTH2(tr, keys[i]);
+            addTD(tr, shapeshift[keys[i]])
+        }
+        body.append(tr);
+    }
+    buildTable();
+    console.log(CLI.bitzBitsqDiff);
 
-//WORK UNDERNEATH HERE --------------------------------------- THIS IS MY TABLE!!!
-
-// $('#myButton').click(function() {
-//     //optionally remove the 500 (which is time in milliseconds) of the
-//     //scrolling animation to remove the animation and make it instant
-//     $.scrollTo($('#myDiv'), 500);
-//  });
-
+});
 //need an on-click to refresh the serverr
 //need an on-click run once 
 // $("#button").on("click",function(){
