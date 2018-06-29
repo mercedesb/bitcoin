a5a11771d3dff9459ae5f7e5a3d3a622f706ad22
Ezra <ezra.cook@u.northwestern.edu>
2018-06-29 14:18:25 -0500

    updates to master

--- keys.js
+++ keys.js
@@ -5,7 +5,8 @@ module.exports = {
     coinigy_secret: process.env.COINIGY_SECRET,
     changelly_key: process.env.CHANGELLY_KEY,
     changelly_secret: process.env.CHANGELLY_SECRET, 
-    firebase_api: process.env.FIREBASE_KEY
+    firebase_api: process.env.FIREBASE_KEY,
+    firebase_auth: process.env.FIREBASE_AUTH
   };
 
   
