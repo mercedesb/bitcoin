7da16ae3b6438faebd6350c59aac2106fe61484b
Ezra <ezra.cook@u.northwestern.edu>
2018-07-01 19:09:48 -0500

    working encryption

--- user/UserController.js
+++ user/UserController.js
@@ -9,7 +9,7 @@ router.use(bodyParser.urlencoded({ extended: true }));
 var User = require('./User');
 
 // CREATES A NEW USER
-router.post('/', function (req, res) {
+router.post('/', VerifyToken, function (req, res) {
     User.create({
             name : req.body.name,
             email : req.body.email,
@@ -22,7 +22,7 @@ router.post('/', function (req, res) {
 });
 
 // RETURNS ALL THE USERS IN THE DATABASE
-router.get('/', function (req, res) {
+router.get('/', VerifyToken, function (req, res) {
     User.find({}, function (err, users) {
         if (err) return res.status(500).send("There was a problem finding the users.");
         res.status(200).send(users);
@@ -30,7 +30,7 @@ router.get('/', function (req, res) {
 });
 
 // GETS A SINGLE USER FROM THE DATABASE
-router.get('/:id', function (req, res) {
+router.get('/:id', VerifyToken, function (req, res) {
     User.findById(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem finding the user.");
         if (!user) return res.status(404).send("No user found.");
@@ -39,7 +39,7 @@ router.get('/:id', function (req, res) {
 });
 
 // DELETES A USER FROM THE DATABASE
-router.delete('/:id', function (req, res) {
+router.delete('/:id', VerifyToken, function (req, res) {
     User.findByIdAndRemove(req.params.id, function (err, user) {
         if (err) return res.status(500).send("There was a problem deleting the user.");
         res.status(200).send("User: "+ user.name +" was deleted.");
